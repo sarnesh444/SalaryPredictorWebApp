d7bfbc3370e766777e2a9c962defdccba4aa07dc
sarnesh444 <46394329+sarnesh444@users.noreply.github.com>
2020-03-01 10:41:29 +0530

    Add files via upload

---
+++ salarypredictor/app.py
@@ -0,0 +1,22 @@
+import numpy as np
+from flask import Flask, request, render_template,url_for
+import pickle
+app = Flask(__name__)
+model = pickle.load(open('models/model.pickle', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    if(request.method=='POST'):
+        int_features = request.form['experience']
+        int_features=[float(i) for i in int_features]
+        final_features = [int_features]
+        print(final_features)
+        prediction = model.predict(final_features)
+        return render_template('index.html', prediction_text='The predicted salary is {}'.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ salarypredictor/scripts/model.py
@@ -0,0 +1,25 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+#import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+# Importing the dataset
+dataset = pd.read_csv(filepath_or_buffer='data/Salary_data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Pickling the trained model:pickling is done to ensure that the model is not trained all over again and the pre trained model can be used
+pickle.dump(regressor, open('model.pickle','wb'))
+model = pickle.load(open('model.pickle','rb'))
